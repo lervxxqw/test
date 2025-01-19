11f0496c27762410a95b557e1e4f0fddd4226e4a
lervxxqw <leraskarevskaa@gmail.com>
2025-01-19 14:03:31 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log("Hello World!")
+function toggleMenu(){
+    console.log("click")
+    const expandMenu = document.getElementById("expand-menu");
+    expandMenu.classList.toggle("show-menu");
+}
