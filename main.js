e84b329eaa27cbb7bc0a4260d175c21df03a27c2
lervxxqw <leraskarevskaa@gmail.com>
2025-01-19 15:13:26 +0200

    first commit

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
-console.log("Hello World!")
+
 function toggleMenu(){
     console.log("click")
     const expandMenu = document.getElementById("expand-menu");
     expandMenu.classList.toggle("show-menu");
 }
+function toggleVikaImg(){
+    const vikaBox = document.getElementById("vika-box");
+    vikaBox.classList.toggle("vika-hide-img");
+
+    const clickMeBtn = document.getElementById("click-me-btn");
+    clickMeBtn.classList.toggle("vika-hide-img");
+}
+
